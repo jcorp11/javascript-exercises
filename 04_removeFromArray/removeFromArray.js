c2dcc0625f96c5eda27198182c3dd1bce2c72937
jcorp11 <joaco.011@gmail.com>
2022-08-04 10:38:58 -0400

    exercises 2 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    let newArray = [];
+    array.forEach(element => {
+        if(!theArgs.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
